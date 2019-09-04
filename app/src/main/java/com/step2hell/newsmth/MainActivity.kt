0c28a8aa44c774f636f1fbefb1353651d10482d3
Bob <40080007@qq.com>
2019-09-04 11:41:44 +0800

    Initial project

---
+++ app/src/main/java/com/step2hell/newsmth/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.step2hell.newsmth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
